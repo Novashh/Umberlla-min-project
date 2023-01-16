86db7ec132eff260a069bd6e15da57fcfcea53ae
Ashish Mali <113760661+Novashh@users.noreply.github.com>
2023-01-16 19:02:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 // @ts-nocheck
-// for color buttons & change umbrella color
 
 let color_Btn = document.querySelectorAll(".color");
+
+let leftImg = document.querySelector("#Left-sect img");
+
+let btnBox = document.querySelector(".btn-box");
+
+let preloader = document.querySelector("#preloader");
+
+let fileInput = document.querySelector("#file");
+
+let uploaded_image = "";
+
+// 1rd --- loading/ preloader for color change/image change
+
+let wind = window.addEventListener("load", hidden);
+
+function hidden() {
+  preloader.classList.add("hidden");
+}
+
+// 2st --for color buttons & change umbrella color
 
 color_Btn.forEach((color) => {
   color.addEventListener("click", () => {
     let colorNameClass = color.className;
+
     if (!color.classList.contains("active-color")) {
       let colorName = colorNameClass.slice(
         colorNameClass.indexOf("-") + 1,
         colorNameClass.length
       );
-
       resetActiveBtn();
       color.classList.add("active-color");
       setNewColor(colorName);
@@ -31,11 +50,11 @@ function resetActiveBtn() {
 // function for image change umbrella and color change button
 
 function setNewColor(color) {
-  document.querySelector("#Left-sect img").src = `./Img/umbrella_${color}.png`;
+  leftImg.src = `./Img/umbrella_${color}.png`;
 }
 
 function btnNewCol(color) {
-  document.querySelector(".btn-box").style.background = `${color}`;
+  btnBox.style.background = `${color}`;
 }
 
 // background color change for diff function
@@ -44,20 +63,7 @@ function changeBodyBg(color) {
   document.body.style.background = color;
 }
 
-// loading/ preloader for color change/image change
-
-let preloader = document.querySelector("#preloader");
-
-window.addEventListener("load", hidden);
-function hidden() {
-  preloader.classList.add("hidden");
-}
-
-// Uploaded image input on umbrella image
-
-let fileInput = document.querySelector("#file");
-
-let uploaded_image = "";
+// 3th --- Uploaded image input on umbrella image
 
 fileInput.addEventListener("change", function () {
   const reader = new FileReader();
@@ -70,12 +76,12 @@ fileInput.addEventListener("change", function () {
   reader.readAsDataURL(this.files[0]);
 });
 
-// button click upload image name show on the button
+// 5th--- button click upload image name show on the button
 
 var loader = function (e) {
   let file = e.target.files;
 
-  let show = "<span>Selected file : </span>" + file[0].name;
+  let show = "<span> Selected file : </span>" + file[0].name;
 
   let output = document.getElementById("selector");
   output.innerHTML = show;
@@ -84,29 +90,30 @@ var loader = function (e) {
 // add event listener for InputEvent
 fileInput.addEventListener("change", loader);
 
-// form validation only png and jpg as well as 5MB file size
+// 6th -- form validation only png and jpg as well as 5MB file size
 
-// var img = document.forms['myform']['img_up'];
+// It works fine but I didn't add
+
+// var img = document.forms["form"]["img_up"];
 
 // var validExt = ["png", "jpg"];
 
 // function validation() {
-//   if (img.value != '') {
-//     var pos_of_dot = img.value.lastIndexOf('.') + 1;
+//   if (img.value != "") {
+//     var pos_of_dot = img.value.lastIndexOf(".") + 1;
 //     var img_ext = img.value.substring(pos_of_dot);
 
-//     var result = validExt.include(img_ext);
+//     var result = validExt.includes(img_ext);
 //     if (result == false) {
+//       alert("false");
 //       return false;
-//     }
-//     else {
+//     } else {
 //       if (parseFloat(img.files[0].size / (1024 * 1024)) >= 5) {
-//         alert('Selected file should be less than 5 MB');
+//         alert("Selected file should be less than 5 MB");
 //         return false;
 //       }
 //     }
-//   }
-//   else {
+//   } else {
 //     return false;
 //   }
 
